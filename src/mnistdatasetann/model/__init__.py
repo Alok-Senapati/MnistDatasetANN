661fb87788a522827e8c384a93c45750d591d247
Alok Senapati <aloksenapati470@gmail.com>
2026-08-30 19:32:10 +0530

    refactor(model): rename model.py to mlp.py and update package exports

--- src/mnistdatasetann/model/__init__.py
+++ src/mnistdatasetann/model/__init__.py
@@ -1,7 +1,7 @@
 """Model classes and training utilities for the MNIST MLP."""
 
 from .loader import load_model
-from .model import MLPClassifier
+from .mlp import MLPClassifier
 from .trainer import evaluate, get_optimizer, get_scheduler, train
 
 __all__ = ["MLPClassifier", "train", "evaluate", "get_optimizer", "get_scheduler", "load_model"]
